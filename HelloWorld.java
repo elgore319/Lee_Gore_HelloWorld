c1035e4ed9e4e4d50bfeda8d59abfff51c0f136a
Eddie Gero <elgore319@gmail.com>
2025-08-25 08:58:32 -0500

    Changed Name

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld { 
+    String name; public HelloWorld(String name) { 
+    this.name = name;
+    } 
+    public void greet() { 
+        System.out.println("Hello, " + name + "!"); 
+    } 
+    public static void main(String[] args) { 
+        HelloWorld student = new HelloWorld("Lee"); student.greet(); 
+    } 
+}
